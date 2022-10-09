1f6c2563974a4df595935e379ee229057a60bb6f
Sem Thijssen <30442@ma-web.nl>
2022-10-09 19:22:54 +0200

    videos 1t/m3 van les 4 gemaakt

---
+++ src/App/App.js
@@ -0,0 +1,10 @@
+import Todo from"../components/Todo/Todo";
+import "./App.css";
+
+const App = () =>{
+    return(
+    <Todo/>
+    )
+}
+
+export default App;
---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,20 @@
+
+import "./Todo.css"
+const Todo = () => {
+    return( 
+<article className="todo">
+    <header class="todo__header">
+        <h1 class="todo__heading">Things to do:</h1>
+    </header>
+    <ul className="todo__list">
+        <li className="todo__item">Groceries</li>
+        <li className="todo__item">Studying</li>
+        <li className="todo__item">Walk the dog </li>
+        <li className="todo__item">Text Girlfriend</li>
+        <li className="todo__item">Code awesome stuff</li>
+    </ul>
+</article>
+    );
+}
+
+export default Todo;
