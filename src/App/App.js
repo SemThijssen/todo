0f02498fa86f97fb39d6a4a81139bc93dca74ecd
Sem Thijssen <30442@ma-web.nl>
2022-10-09 20:17:03 +0200

    video 7 van les 4 afgemaakt

--- src/App/App.js
+++ src/App/App.js
@@ -2,15 +2,8 @@ import Todo from"../components/Todo/Todo";
 import "./App.css";
 
 const App = () =>{
-    const tasks =  [
-        "Groceries",
-        "Walking the dog",
-        "Studying",
-        "Message girlfriend",
-        "Code awesome stuff",
-    ];
     return(
-    <Todo tasks ={tasks}/>
+    <Todo/>
     )
 }
 
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,21 +1,39 @@
 
 import "./Todo.css"
-const Todo = (props) => {
-    let double = props.tasks.map( task => {
-        return task + task;
+const Todo = () => {
+    const tasks =  [
+        "Groceries",
+        "Walking the dog",
+        "Studying",
+        "Message girlfriend",
+        "Code awesome stuff",
+        "Code more awesome stuff",
+        "Groceries",
+        "Walking the dog",
+        "Studying",
+        "Message girlfriend",
+        "Code awesome stuff",
+        "Code more awesome stuff",
+        "Groceries",
+        "Walking the dog",
+        "Studying",
+        "Message girlfriend",
+        "Code awesome stuff",
+        "Code more awesome stuff",
+    ];
+    let listItems = tasks.map( task => {
+        return <li class="todo__item">{task}</li>
     });
-    console.log(double);
+   
+
+    console.log(listItems);
     return(
 <article className="todo">
     <header class="todo__header">
         <h1 class="todo__heading">Things to do:</h1>
     </header>
     <ul className="todo__list">
-        <li className="todo__item">Groceries</li>
-        <li className="todo__item">Studying</li>
-        <li className="todo__item">Walk the dog </li>
-        <li className="todo__item">Text Girlfriend</li>
-        <li className="todo__item">Code awesome stuff</li>
+       {listItems}
     </ul>
 </article>
     );
