7ae10516b652672735f392dca4bcb21a92134fb4
Sem Thijssen <30442@ma-web.nl>
2022-10-11 14:19:53 +0200

    video 4 en 5 gemaakt van les 5

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -26,18 +26,35 @@ import "./Todo.css";
    
 componentDidUpdate(){
 console.log("vanuit de update");
-console.log(this.state.tasks);
+
 
 }
+
+update = () => {
+    console.log("Ik ben geklikt");
+    this.setState({
+        tasks: this.state.tasks.push(
+            {
+                name:"Groceries",
+                done: true,
+                id: 1
+            },
+        )
+    })
+}
     render(){
+        console.log(this.state.tasks);
+    let items = this.state.tasks.map(task =>{
+        return <Item name={task.name} key={task.id}></Item>
+    });
         console.log("Vanuit de render!");
         return (
-            <article className="todo">
+            <article onClick={this.update}className="todo">
                 <header className="todo__header">
                     <h1 className="todo__heading">muzieklijst</h1>
                 </header>
                 <ul className="todo__list">
-                   {this.listItems}
+                   {items}
                 </ul>
             </article>
                 );
